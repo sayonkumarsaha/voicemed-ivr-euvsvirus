29d2df3dd88244a4c0f73b23e908c5c650f6a964
Sayon Saha <sayonkumarsaha@gmail.comn>
2020-04-26 16:11:14 +0200

    added all data types recording

--- src/process_recordings.py
+++ src/process_recordings.py
@@ -1,21 +1,40 @@
 from flask import Flask, request
 from google.cloud import storage
-import urllib.request
+import json
+import requests
+import os
 
 app = Flask(__name__)
 
-HOST = 'localhost'
-PORT = 5002
+FIREBASE_CREDENTIAL = '../voicemed_storage.json'
+FIREBASE_BUCKET = 'voicemedcalls-78bd8.appspot.com'
 
-#CREDENTIAL_FILE = './VoiceMed-4b6f991d26a8.json.json'
-#BUCKET = 'voicemed-9f3f6.appspot.com'
 
-STORAGE_CREDENTIAL = './voicemed_storage_credentials.json'
-BUCKET= 'voicemedcalls-78bd8.appspot.com'
+def _write_record_data(data, data_type, call_sid, rec_sid, rec_url):
+    """Writes data on Firebase storage """
+
+    storage_client = storage.Client.from_service_account_json(FIREBASE_CREDENTIAL)
+    bucket = storage_client.get_bucket(FIREBASE_BUCKET)
+
+    # TODO: Add headers for the CSV from _fetch_record_data
+    filename = str(call_sid) + "_" + data_type + '.txt'
+    blob = bucket.blob(filename)
+    blob.upload_from_string(data)
+
+    # Downloading the recording URL and storing it on Firebase
+    wav_filename = str(call_sid) + '_' + rec_sid + "_" + data_type + '.wav'
+    blob = bucket.blob(wav_filename)
+    r = requests.get(rec_url)
+    with open(wav_filename, 'wb') as f:
+        f.write(r.content)
+    with open(wav_filename, 'rb') as f:
+        blob.upload_from_file(f)
+    if os.path.exists(wav_filename):
+        os.remove(wav_filename)
 
 
 def _fetch_record_data(request):
-    """Fetches voice recordings from Studio given the request """
+    """Fetches voice recordings data from Studio request """
 
     rec_sid = request.values.get('RecordingSid')
 
@@ -39,48 +58,79 @@ def _fetch_record_data(request):
                         + str(rec_channels) + separator \
                         + str(rec_start_time) + separator \
                         + str(rec_source)
-        return rec_sid, call_sid, recorded_data
+        return rec_sid, call_sid, rec_url, recorded_data
 
-    return rec_sid, None, None
+    return rec_sid, None, None, None
 
+
+# TODO: Instead of storing a separate file for each category, combine them in a csv and store one file per call_id
 
 @app.route("/fetch-consent-recording", methods=['GET', 'POST'])
 def fetch_consent_recording():
-    """Fetches consent recordings from Studio and stores in Firebase Storage."""
+    """Fetches consent recordings from Studio and stores in Firebase Storage"""
 
-    TYPE = 'consent'
-    rec_sid, call_sid, consent_data = _fetch_record_data(request)
-
+    data_type = 'consent'
+    rec_sid, call_sid, rec_url, consent_data = _fetch_record_data(request)
     if rec_sid:
-        filename = TYPE + '_call_sid_' + str(call_sid) + '_rec_sid_' + str(rec_sid) + '.txt'
-        storage_client = storage.Client.from_service_account_json(STORAGE_CREDENTIAL)
-        bucket = storage_client.get_bucket(BUCKET)
-        blob = bucket.blob(filename)
-        blob.upload_from_string(consent_data)
-
-        return "Successfully stored Consent Recording in Firebase Storage"
+        _write_record_data(consent_data, data_type, call_sid, rec_sid, rec_url)
+        return "Successfully stored " + data_type + " recording in firebase storage"
 
     return "No recordings processed"
 
 
 @app.route("/fetch-health-recording", methods=['GET', 'POST'])
 def fetch_health_recording():
-    """Fetches consent recordings from Studio and stores in Firebase Storage."""
+    """Fetches health recordings from Studio and stores in Firebase Storage"""
 
-    TYPE = 'health'
-    rec_sid, call_sid, health_data = _fetch_record_data(request)
-
+    data_type = 'health'
+    rec_sid, call_sid, rec_url, consent_data = _fetch_record_data(request)
     if rec_sid:
-        filename = TYPE + '_call_sid_' + str(call_sid) + '_rec_sid_' + str(rec_sid) + '.txt'
-        storage_client = storage.Client.from_service_account_json(STORAGE_CREDENTIAL)
-        bucket = storage_client.get_bucket(BUCKET)
-        blob = bucket.blob(filename)
-        blob.upload_from_string(health_data)
+        _write_record_data(consent_data, data_type, call_sid, rec_sid, rec_url)
+        return "Successfully stored " + data_type + " recording in firebase storage"
 
-        return "Successfully stored Health Recording in Firebase Storage"
+    return "No recordings processed"
+
+
+@app.route("/fetch-cough-recording", methods=['GET', 'POST'])
+def fetch_cough_recording():
+    """Fetches cough recordings from Studio and stores in Firebase Storage"""
+
+    data_type = 'cough'
+    rec_sid, call_sid, rec_url, consent_data = _fetch_record_data(request)
+    if rec_sid:
+        _write_record_data(consent_data, data_type, call_sid, rec_sid, rec_url)
+        return "Successfully stored " + data_type + " recording in firebase storage"
+
+    return "No recordings processed"
+
+
+@app.route("/fetch-breath-recording", methods=['GET', 'POST'])
+def fetch_breath_recording():
+    """Fetches breath recordings from Studio and stores in Firebase Storage"""
+
+    data_type = 'breath'
+    rec_sid, call_sid, rec_url, consent_data = _fetch_record_data(request)
+    if rec_sid:
+        _write_record_data(consent_data, data_type, call_sid, rec_sid, rec_url)
+        return "Successfully stored " + data_type + " recording in firebase storage"
+
+    return "No recordings processed"
+
+
+@app.route("/fetch-speech-recording", methods=['GET', 'POST'])
+def fetch_speech_recording():
+    """Fetches speech recordings from Studio and stores in Firebase Storage"""
+
+    data_type = 'speech'
+    rec_sid, call_sid, rec_url, consent_data = _fetch_record_data(request)
+    if rec_sid:
+        _write_record_data(consent_data, data_type, call_sid, rec_sid, rec_url)
+        return "Successfully stored " + data_type + " recording in firebase storage"
 
     return "No recordings processed"
 
 
 if __name__ == "__main__":
-    app.run(host = HOST, port = PORT, debug = True)
+    app.run(host = 'localhost', port = 5002, debug = True)
+    # Threaded option to enable multiple instances for multiple user access support
+    # app.run(threaded=True, port=5002)
