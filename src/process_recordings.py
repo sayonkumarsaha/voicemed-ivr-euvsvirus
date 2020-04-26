440ad0a6728dcbf22421094b9820a2ca6544ac83
Sayon Saha <sayonkumarsaha@gmail.comn>
2020-04-26 14:09:06 +0200

    updated recording storage

--- src/process_recordings.py
+++ src/process_recordings.py
@@ -1,43 +1,85 @@
 from flask import Flask, request
 from google.cloud import storage
+import urllib.request
 
 app = Flask(__name__)
 
 HOST = 'localhost'
 PORT = 5002
 
-CREDENTIAL_FILE = './VoiceMed-4b6f991d26a8.json'
-BUCKET = 'voicemed-9f3f6.appspot.com'
+#CREDENTIAL_FILE = './VoiceMed-4b6f991d26a8.json.json'
+#BUCKET = 'voicemed-9f3f6.appspot.com'
+
+STORAGE_CREDENTIAL = './voicemed_storage_credentials.json'
+BUCKET= 'voicemedcalls-78bd8.appspot.com'
 
 
-@app.route("/fetch-call-recording", methods=['GET', 'POST'])
-def fetch_call_recording():
-    """Fetches voice recordings from Studio and stores in Firebase Storage."""
+def _fetch_record_data(request):
+    """Fetches voice recordings from Studio given the request """
 
-    acc_sid = request.values.get('AccountSid')
-    call_sid = request.values.get('CallSid')
     rec_sid = request.values.get('RecordingSid')
-    rec_url = request.values.get('RecordingUrl')
-    rec_status = request.values.get('RecordingStatus')
-    rec_duration = request.values.get('RecordingDuration')
-    rec_channels = request.values.get('RecordingChannels')
-    rec_start_time = request.values.get('RecordingStartTime')
-    rec_source = request.values.get('RecordingSource')
 
-    recording_data = (acc_sid, call_sid, rec_sid, rec_url, rec_status, rec_duration, rec_channels, rec_start_time,
-                      rec_source)
+    if rec_sid:
+        acc_sid = request.values.get('AccountSid')
+        call_sid = request.values.get('CallSid')
+        rec_url = request.values.get('RecordingUrl')
+        rec_status = request.values.get('RecordingStatus')
+        rec_duration = request.values.get('RecordingDuration')
+        rec_channels = request.values.get('RecordingChannels')
+        rec_start_time = request.values.get('RecordingStartTime')
+        rec_source = request.values.get('RecordingSource')
 
-    print(recording_data)
+        separator = ','
+        recorded_data = str(acc_sid) + separator \
+                        + str(call_sid) + separator \
+                        + str(rec_sid) + separator \
+                        + str(rec_url) + separator \
+                        + str(rec_status) + separator \
+                        + str(rec_duration) + separator \
+                        + str(rec_channels) + separator \
+                        + str(rec_start_time) + separator \
+                        + str(rec_source)
+        return rec_sid, call_sid, recorded_data
 
-    # Explicitly use service account credentials by specifying the private key file.
-    storage_client = storage.Client.from_service_account_json(CREDENTIAL_FILE)
-    print(storage_client)
+    return rec_sid, None, None
 
-    bucket = storage_client.get_bucket(BUCKET)
-    blob = bucket.blob('rec_id_'+str(rec_sid)+'.txt')
-    blob.upload_from_string(''.join(recording_data))
 
-    return "Successfully stored in Firebase Storage"
+@app.route("/fetch-consent-recording", methods=['GET', 'POST'])
+def fetch_consent_recording():
+    """Fetches consent recordings from Studio and stores in Firebase Storage."""
+
+    TYPE = 'consent'
+    rec_sid, call_sid, consent_data = _fetch_record_data(request)
+
+    if rec_sid:
+        filename = TYPE + '_call_sid_' + str(call_sid) + '_rec_sid_' + str(rec_sid) + '.txt'
+        storage_client = storage.Client.from_service_account_json(STORAGE_CREDENTIAL)
+        bucket = storage_client.get_bucket(BUCKET)
+        blob = bucket.blob(filename)
+        blob.upload_from_string(consent_data)
+
+        return "Successfully stored Consent Recording in Firebase Storage"
+
+    return "No recordings processed"
+
+
+@app.route("/fetch-health-recording", methods=['GET', 'POST'])
+def fetch_health_recording():
+    """Fetches consent recordings from Studio and stores in Firebase Storage."""
+
+    TYPE = 'health'
+    rec_sid, call_sid, health_data = _fetch_record_data(request)
+
+    if rec_sid:
+        filename = TYPE + '_call_sid_' + str(call_sid) + '_rec_sid_' + str(rec_sid) + '.txt'
+        storage_client = storage.Client.from_service_account_json(STORAGE_CREDENTIAL)
+        bucket = storage_client.get_bucket(BUCKET)
+        blob = bucket.blob(filename)
+        blob.upload_from_string(health_data)
+
+        return "Successfully stored Health Recording in Firebase Storage"
+
+    return "No recordings processed"
 
 
 if __name__ == "__main__":
